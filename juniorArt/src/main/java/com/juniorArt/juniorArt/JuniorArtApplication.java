0a7ee8efcd3e2df25a1453b1475d6e36f43be465
20161204010015 <20161204010015@LAJ291347.ifrn.local>
2019-11-26 10:26:26 -0200

    Clonando o Projeto

---
+++ juniorArt/src/main/java/com/juniorArt/juniorArt/JuniorArtApplication.java
@@ -0,0 +1,13 @@
+package com.juniorArt.juniorArt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorArtApplication.class, args);
+	}
+
+}
---
+++ juniorArt/src/test/java/com/juniorArt/juniorArt/JuniorArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniorArt.juniorArt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
